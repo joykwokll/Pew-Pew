e8cea8f78742589078cfc5d06c1129d1ad714a5f
joykwokk <lianglilnf-1996@hotmail.com>
2021-12-12 16:47:41 +0800

    target board and score

--- index.js
+++ index.js
@@ -1,12 +1,45 @@
+const startButton = document.querySelector(".startButton");
+
+let score = 0;
+
+startButton.addEventListener("click", () => {
+    container.appendChild(targetBoard);
+
+    startButton.innerText = "SCORE : " + score;
+})
+const container = document.querySelector(".container");
+// const targetBoard = document.createElement("img");
+// targetBoard.setAttribute("class", "targetBoard");
+// targetBoard.setAttribute("src", "./Target Board.png");
+const targetBoard = document.querySelector(".target");
+
+const contHeight = container.offsetHeight;
+const contWidth = container.offsetWidth;
+
+setInterval(() => {
+    const randTop = Math.random() * (contHeight - 100);
+    const randLeft = Math.random() * (contWidth - 100);    
+
+    targetBoard.style.position = "absolute";
+    targetBoard.style.top = randTop + "px";
+    targetBoard.style.left = randLeft + "px";
+}, 1000);
+
 const bullet = document.querySelector(".bullet");
 
 window.addEventListener("click", (e) => {
     console.log(e);
     bullet.style.top = e.pageY + "px"
     bullet.style.left = e.pageX + "px"
+
+    if(e.target === targetBoard) {
+        score++ 
+    } else { score-- }
+    startButton.innerHTML = "SCORE : " + score;
 });
 
 const cursor = document.querySelector(".cursor");
+
 window.addEventListener("mousemove", (e) => {
     cursor.style.top = e.pageY + "px";
     cursor.style.left = e.pageX + "px";
