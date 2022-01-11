de8bf9f840232cb57659f1e68f6eb9f9efdbb9b2
joykwokk <lianglilnf-1996@hotmail.com>
2022-01-11 18:39:40 +0800

    still solving score problem

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const startButton = document.querySelector(".startButton");
+let started = false;
 
 let score = 0;
 
+function startGame () {
+    console.log("started");
+    if (!started) {
+        setInterval(() => {
+            const randTop = Math.random() * (contHeight - 100);
+            const randLeft = Math.random() * (contWidth - 100);    
+
+            targetBoard.style.position = "absolute";
+            targetBoard.style.top = randTop + "px";
+            targetBoard.style.left = randLeft + "px";
+        }, 1500);
+    }
+    started = true;
+}
+
 startButton.addEventListener("click", () => {
     container.appendChild(targetBoard);
-
     startButton.innerText = "SCORE : " + score;
+    startGame();
 })
 const container = document.querySelector(".container");
 // const targetBoard = document.createElement("img");
@@ -16,31 +32,25 @@ const targetBoard = document.querySelector(".target");
 const contHeight = container.offsetHeight;
 const contWidth = container.offsetWidth;
 
-setInterval(() => {
-    const randTop = Math.random() * (contHeight - 100);
-    const randLeft = Math.random() * (contWidth - 100);    
-
-    targetBoard.style.position = "absolute";
-    targetBoard.style.top = randTop + "px";
-    targetBoard.style.left = randLeft + "px";
-}, 1000);
-
 const bullet = document.querySelector(".bullet");
 
 window.addEventListener("click", (e) => {
-    console.log(e);
     bullet.style.top = e.pageY + "px"
     bullet.style.left = e.pageX + "px"
+    setTimeout(() => {
+        bullet.style.top = "0px"
+        bullet.style.left = "0px"
+    }, 500);
 
     if(e.target === targetBoard) {
         score++ 
-    } else { score-- }
-    startButton.innerHTML = "SCORE : " + score;
+    } else { score > 0 && score-- }
+//     if (started) startButton.innerHTML = "SCORE : " + score;
 });
 
-const cursor = document.querySelector(".cursor");
+// const cursor = document.querySelector(".cursor");
 
-window.addEventListener("mousemove", (e) => {
-    cursor.style.top = e.pageY + "px";
-    cursor.style.left = e.pageX + "px";
-});
+// window.addEventListener("mousemove", (e) => {
+//     cursor.style.top = e.pageY + "px";
+//     cursor.style.left = e.pageX + "px";
+// });
