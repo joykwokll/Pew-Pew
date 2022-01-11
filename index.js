e1635ef69f36ad80ce4a0336ee2a59adcaaf42b8
joykwokk <lianglilnf-1996@hotmail.com>
2022-01-11 20:25:38 +0800

    game start condition

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
 const startButton = document.querySelector(".startButton");
+const container = document.querySelector(".container");
+const bullet = document.querySelector(".bullet");
 let started = false;
 
 let score = 0;
 
-function startGame () {
-    console.log("started");
-    if (!started) {
-        setInterval(() => {
-            const randTop = Math.random() * (contHeight - 100);
-            const randLeft = Math.random() * (contWidth - 100);    
-
-            targetBoard.style.position = "absolute";
-            targetBoard.style.top = randTop + "px";
-            targetBoard.style.left = randLeft + "px";
-        }, 1500);
-    }
-    started = true;
-}
-
-startButton.addEventListener("click", () => {
-    container.appendChild(targetBoard);
-    startButton.innerText = "SCORE : " + score;
-    startGame();
-})
-const container = document.querySelector(".container");
-// const targetBoard = document.createElement("img");
-// targetBoard.setAttribute("class", "targetBoard");
-// targetBoard.setAttribute("src", "./Target Board.png");
-const targetBoard = document.querySelector(".target");
-
 const contHeight = container.offsetHeight;
 const contWidth = container.offsetWidth;
 
-const bullet = document.querySelector(".bullet");
+function startGame() {
+    console.log("started");
+    started = true;
+    startButton.remove();
+    const targetBoard = document.createElement("img");
+    targetBoard.setAttribute("class", "targetBoard");
+    targetBoard.setAttribute("src", "./Target Board.png");
+    container.appendChild(targetBoard);
+    setInterval(() => {
+        const randTop = Math.random() * (contHeight - 100);
+        const randLeft = Math.random() * (contWidth - 100);
 
-window.addEventListener("click", (e) => {
-    bullet.style.top = e.pageY + "px"
-    bullet.style.left = e.pageX + "px"
-    setTimeout(() => {
-        bullet.style.top = "0px"
-        bullet.style.left = "0px"
-    }, 500);
+        targetBoard.style.position = "absolute";
+        targetBoard.style.top = randTop + "px";
+        targetBoard.style.left = randLeft + "px";
+    }, 1500);
+    window.addEventListener("click", (e) => {
+        bullet.style.top = e.pageY + "px"
+        bullet.style.left = e.pageX + "px"
+        // setTimeout(() => {
+        //     bullet.style.top = "0px"
+        //     bullet.style.left = "0px"
+        // }, 500);
+        console.log(e.pageY)
+        console.log(e.pageX)
+        console.log(bullet.style.top)
+        console.log(bullet.style.left)
 
-    if(e.target === targetBoard) {
-        score++ 
-    } else { score > 0 && score-- }
-//     if (started) startButton.innerHTML = "SCORE : " + score;
-});
+
+        if (e.target === targetBoard) {
+            score++
+        } else { score > 0 && score-- }
+        //     if (started) startButton.innerHTML = "SCORE : " + score;
+    });
+}
+
+startButton.addEventListener("click", (event) => {
+    console.log(event.currentTarget);
+    // startButton.innerText = "SCORE : " + score;
+    startGame();
+})
 
 // const cursor = document.querySelector(".cursor");
-
 // window.addEventListener("mousemove", (e) => {
 //     cursor.style.top = e.pageY + "px";
 //     cursor.style.left = e.pageX + "px";
