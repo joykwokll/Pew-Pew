199d3c85a5cc143a8a5c906d88314f9cee6849fa
joykwokk <lianglilnf-1996@hotmail.com>
2022-01-13 21:49:11 +0800

    edited targetboard image

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ const contHeight = container.offsetHeight;
 const contWidth = container.offsetWidth;
 
 //TIMER FUNCTION//
+let startingTime = 10;
 function timerCountdown(){
-    let startingTime = 10;
-    setInterval(startingTime --, 10000);
-    document.querySelector(".timer").innerHTML = "TIMER: " + startingTime + " Seconds"
+    let timer = setInterval(() => {
+        startingTime--;
+        document.querySelector(".timer").innerHTML = "TIMER: " + startingTime + " Seconds"
+    }, 1000);
+    // if(startingTime <= 0) {
+    //     clearInterval();
+    // }
+}
+function stopTimer(){
+    if(startingTime <= 0) {
+        clearInterval();
+    }
 }
 
 function startGame(level) {
     console.log("started");
     started = true;
     startButton.remove();
+
+    //TARGET BOARD//
     const targetBoard = document.createElement("img");
     targetBoard.setAttribute("class", "targetBoard");
     targetBoard.setAttribute("src", "./targetboard.png");
@@ -30,6 +42,9 @@ function startGame(level) {
         targetBoard.style.top = randTop + "px";
         targetBoard.style.left = randLeft + "px";
     }, level);
+    //TARGET BOARD//
+
+    //
     window.addEventListener("click", (e) => {
         bullet.style.top = e.pageY + "px"
         bullet.style.left = e.pageX + "px"
@@ -37,6 +52,7 @@ function startGame(level) {
             bullet.style.top = "0px"
             bullet.style.left = "0px"
         }, 500);
+
         // let bulletTop = bullet.offsetTop
 
         console.log(e.pageY)
@@ -54,7 +70,7 @@ function startGame(level) {
         document.querySelector(".score").innerHTML = "SCORE: " + score
         //     if (started) startButton.innerHTML = "SCORE : " + score;
     });
-
+    timerCountdown();
 
 }
 
